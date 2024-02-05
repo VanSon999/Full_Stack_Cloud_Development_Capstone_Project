3d70656b51d86b37cfeeee8d888eab02d5e4201c
sonnguyen <nguyenvanson95999@gmail.com>
2024-02-04 20:02:57 -0500

    add authenticate

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,12 +12,14 @@ urlpatterns = [
     # path for about view
     path(route='about', view=views.about, name='about'),
     # path for contact us view
-    path(route='contact', view=views.contact, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
+    path(route='registration', view=views.registration_request, name='registration'),
 
     # path for login
-
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
